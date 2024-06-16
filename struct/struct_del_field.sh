3b44a0be368d45fa372028e7c58fada454f4145d
QinQing <northisland2017@gmail.com>
2024-06-16 10:27:22 +0800

    FIXED: struct_del_field

--- struct/struct_del_field.sh
+++ struct/struct_del_field.sh
@@ -1,6 +1,8 @@
 . ./meta/meta.sh
 ((DEFENSE_VARIABLES[struct_del_field]++)) && return 0
 
+# . ./log/log_dbg.sh || return 1
+
 # 删除不需要指定键的类型
 # struct_del_field 'struct_name' 'filed_name' 'key1' 0 4
 # 结构体复合变量创建
@@ -17,7 +19,7 @@ struct_del_field ()
     fi
 
     # 不是顶级键的情况,从上到下查找
-    eval local _struct_del_field_data_lev{1..20}=''
+    eval local _struct_set_field_data_lev{1..20}=''
     # 记录每层需要更新的索引(0层不处理先占位)
     local -a _struct_del_field_index_lev=('')
     local _struct_del_field_index _struct_del_field_top_level_str=''
@@ -28,33 +30,38 @@ struct_del_field ()
     for _struct_del_field_index in "${@:1:$#}" ; do
         ((_struct_del_field_lev_cnt++))
         [[ -z "$_struct_del_field_index" ]] && return 1
-        local -n _struct_del_field_data_lev_ref=_struct_del_field_data_lev${_struct_del_field_lev_cnt}
+        local -n _struct_del_field_data_lev_ref=_struct_set_field_data_lev${_struct_del_field_lev_cnt}
 
         # 转换上一个数据类型
         if [[ "$_struct_del_field_data_lev_ref_last" =~ ^(declare)\ ([^\ ]+)\ _struct_set_field_data_lev[0-9]+=(.*) ]] ; then
-            declare ${BASH_REMATCH[2]} _struct_del_field_data_lev${_struct_del_field_lev_cnt}
+            declare ${BASH_REMATCH[2]} _struct_set_field_data_lev${_struct_del_field_lev_cnt}
             eval _struct_del_field_data_lev_ref="${BASH_REMATCH[3]}"
+            # log_dbg d "_struct_set_field_data_lev${_struct_del_field_lev_cnt}" _struct_set_field_data_lev${_struct_del_field_lev_cnt}
         fi
 
         _struct_del_field_data_lev_ref_last="${_struct_del_field_data_lev_ref["$_struct_del_field_index"]}"
 
         _struct_del_field_index_lev+=("$_struct_del_field_index")
-
     done
+
+    _struct_del_field_index_lev=('' "${_struct_del_field_index_lev[@]}")
+
+    # log_dbg d "_struct_del_field_index_lev _struct_del_field_lev_cnt" _struct_del_field_index_lev _struct_del_field_lev_cnt
 
     # 删除最后一级,然后依次往上更新
     local -i _struct_del_field_is_not_last_lev=0
-    while ((_struct_del_field_lev_cnt>1)) ; do
+    while ((_struct_del_field_lev_cnt>2)) ; do
         # 取出当前层数据结构
-        local -n _struct_del_field_data_lev_ref=_struct_del_field_data_lev${_struct_del_field_lev_cnt}
+        local -n _struct_del_field_data_lev_ref=_struct_set_field_data_lev$((_struct_del_field_lev_cnt-1))
 
         if ! ((_struct_del_field_is_not_last_lev++)) ; then
             # 删除底级键
-            unset '_struct_del_field_data_lev_ref["${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}"]'
+            # log_dbg d "_struct_del_field_data_lev_ref" _struct_del_field_data_lev_ref 
+            unset '_struct_del_field_data_lev_ref[${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}]'
         else
             # 如果上一级删除的结果导致了空数组或者空hash,那么继续删除
             if [[ -z "$_struct_del_field_top_level_str" ]] ; then
-                unset '_struct_del_field_data_lev_ref["${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}"]'
+                unset '_struct_del_field_data_lev_ref[${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}]'
             fi
         fi
         
