e4455e5544581a7075544374c85e17f7182f5b68
QinQing <northisland2017@gmail.com>
2024-06-16 23:12:03 +0800

    WIP: del

--- struct/struct_del_field.sh
+++ struct/struct_del_field.sh
@@ -1,7 +1,7 @@
 . ./meta/meta.sh
 ((DEFENSE_VARIABLES[struct_del_field]++)) && return 0
 
-# . ./log/log_dbg.sh || return 1
+. ./log/log_dbg.sh || return 1
 
 # 删除不需要指定键的类型
 # struct_del_field 'struct_name' 'filed_name' 'key1' 0 4
@@ -27,10 +27,12 @@ struct_del_field ()
     local _struct_del_field_index_first="${1}"
     local _struct_del_field_data_lev_ref_last=${_struct_del_field_struct_ref["$_struct_del_field_index_first"]}
 
-    for _struct_del_field_index in "${@:1:$#}" ; do
+    for _struct_del_field_index in "${@:2:$#}" ; do
         ((_struct_del_field_lev_cnt++))
         [[ -z "$_struct_del_field_index" ]] && return 1
         local -n _struct_del_field_data_lev_ref=_struct_set_field_data_lev${_struct_del_field_lev_cnt}
+        
+        # log_dbg d '' _struct_del_field_data_lev_ref_last
 
         # 转换上一个数据类型
         if [[ "$_struct_del_field_data_lev_ref_last" =~ ^(declare)\ ([^\ ]+)\ _struct_set_field_data_lev[0-9]+=(.*) ]] ; then
@@ -39,12 +41,15 @@ struct_del_field ()
             # log_dbg d "_struct_set_field_data_lev${_struct_del_field_lev_cnt}" _struct_set_field_data_lev${_struct_del_field_lev_cnt}
         fi
 
+        log_dbg d '' _struct_del_field_data_lev_ref _struct_del_field_index
         _struct_del_field_data_lev_ref_last="${_struct_del_field_data_lev_ref["$_struct_del_field_index"]}"
 
         _struct_del_field_index_lev+=("$_struct_del_field_index")
+        # log_dbg d '' _struct_del_field_data_lev_ref
     done
 
     _struct_del_field_index_lev=('' "${_struct_del_field_index_lev[@]}")
+    # log_dbg d '' _struct_del_field_index_lev 
 
     # log_dbg d "_struct_del_field_index_lev _struct_del_field_lev_cnt" _struct_del_field_index_lev _struct_del_field_lev_cnt
 
@@ -56,12 +61,15 @@ struct_del_field ()
 
         if ! ((_struct_del_field_is_not_last_lev++)) ; then
             # 删除底级键
-            # log_dbg d "_struct_del_field_data_lev_ref" _struct_del_field_data_lev_ref 
+            log_dbg d "_struct_del_field_data_lev_ref" _struct_del_field_data_lev_ref 
             unset '_struct_del_field_data_lev_ref[${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}]'
         else
             # 如果上一级删除的结果导致了空数组或者空hash,那么继续删除
             if [[ -z "$_struct_del_field_top_level_str" ]] ; then
                 unset '_struct_del_field_data_lev_ref[${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}]'
+            else
+                # 如果不是空的,更新索引
+                _struct_del_field_data_lev_ref["${_struct_del_field_index_lev[_struct_del_field_lev_cnt]}"]="$_struct_del_field_top_level_str"
             fi
         fi
         
