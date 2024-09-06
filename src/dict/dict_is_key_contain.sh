178b8bce3145cd8b0e2364a5d2cde48f96ea8d20
QinQing <northisland2017@gmail.com>
2024-09-07 00:43:31 +0800

    ADDED: dict_grep_key_contain dict_is_key_contain

---
+++ src/dict/dict_is_key_contain.sh
@@ -0,0 +1,22 @@
+. ./meta/meta.sh
+((DEFENSE_VARIABLES[dict_is_key_contain]++)) && return 0
+
+# 判断关联数组的键是否包含一个字符串
+dict_is_key_contain ()
+{
+    local _script_i${1}='
+        local i'$1'
+        local -i i'$1'_contain=0
+        for i'$1' in "${!'$1'[@]}"; do
+            if [[ "${i'$1'}" == *"${2}"* ]] ; then
+                i'$1'_contain=1
+                break
+            fi
+        done
+        ((i'$1'_contain))
+    '
+    eval -- eval -- \"\$"_script_i${1}"\"
+}
+
+return 0
+
