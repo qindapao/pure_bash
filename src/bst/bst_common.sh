1de9751fe1fc142f6451fafcfaa346bb141c7d9e
qindapao <northisland2017@gmail.com>
2024-11-03 20:54:20 +0800

    ADDED: bst_del

--- src/bst/bst_common.sh
+++ src/bst/bst_common.sh
@@ -5,6 +5,7 @@ declare -gA BST_COMMON_ERR_DEFINE=()
 
 # 每一种操作留8个状态码
 # insert 8~15
+# del 16~23
 
 BST_COMMON_ERR_DEFINE[ok]=0
 
@@ -14,6 +15,10 @@ BST_COMMON_ERR_DEFINE[insert_null_tree]=8
 # 插入重复元素
 BST_COMMON_ERR_DEFINE[insert_same_e]=9
 
+# 删除的节点未找到
+BST_COMMON_ERR_DEFINE[del_not_found]=16
+# 删除的节点是根节点
+BST_COMMON_ERR_DEFINE[del_root_node]=17
 
 return 0
 
--- src/bst/bst_del.sh
+++ src/bst/bst_del.sh
@@ -2,44 +2,122 @@
 ((DEFENSE_VARIABLES[bst_del]++)) && return 0
 
 
-# 二叉树插入一个节点
-bst_del () 
-{ 
-    local -n _bst_del_tree_ref=$1
-    local _bst_del_node=$2
-
-    if [[ -z "${_bst_del_tree_ref[$]}" ]]
-    
-
-    local -n _bst_insert_tree_ref=$1
-    local -n _bst_insert_tree_id_ref=$2
-    local _bst_insert_parent=$3
-    local _bst_insert_direction=$4
-    local _bst_insert_value=$5
-    local _bst_insert_node_id=
-    local _bst_insert_recycle_ids
-    
-    # 插入一个新节点
-    if [[ -n "${_bst_insert_tree_ref[recycle_ids]}" ]] ; then
-        _bst_insert_node_id=${_bst_insert_recycle_ids##* }
-        _bst_insert_recycle_ids=${_bst_insert_recycle_ids% *}
-        # 重新生成数组的方法
-        # printf -v m "(%s)" "${a[*]@Q}"
-    else
-        _bst_insert_node_id=$((_bst_insert_tree_ref[idcount]++))
-    fi
-
-    _bst_insert_tree_ref[${_bst_insert_node_id}.left]=0
-    _bst_insert_tree_ref[${_bst_insert_node_id}.right]=0
-    _bst_insert_tree_ref[${_bst_insert_node_id}.value]=$_bst_insert_value
-    # 挂接父节点
-    _bst_insert_tree_ref[${_bst_insert_parent}.${_bst_insert_direction}]=$_bst_insert_node_id
-    # 保存父节点的引用和方向
-    _bst_insert_tree_ref[${_bst_insert_node_id}.parent]=$_bst_insert_parent
-    _bst_insert_tree_ref[${_bst_insert_node_id}.parent_side]=$_bst_insert_direction
-    # 返回节点ID
-    _bst_insert_tree_id_ref=$_bst_insert_node_id
+_bst_del_node ()
+{
+    local -n __bst_del_node_ref="$1"
+    local __bst_del_node_id="$2"
+    unset -v '__bst_del_node_ref[${__bst_del_node_id}]' \
+        '__bst_del_node_ref[${__bst_del_node_id}.left]' \
+        '__bst_del_node_ref[${__bst_del_node_id}.right]' \
+        '__bst_del_node_ref[${__bst_del_node_id}.value]' \
+        '__bst_del_node_ref[${__bst_del_node_id}.parent]' \
+        '__bst_del_node_ref[${__bst_del_node_id}.parent_side]'
+    __bst_del_node_ref[recycle_ids]+=" ${__bst_del_node_id}"
 }
 
-return 0
+# 二叉树删除一个节点
+bst_del () 
+{
+    local -n _bst_del_ref=$1
+    local _bst_del_root_id=$2
+    local _bst_del_out_id=$3
+    local _bst_del_value=$4
+    local _bst_del_current=$_bst_del_root_id
+    local _bst_del_parent=0
+    local _bst_del_is_l_child=1
+    local _bst_del_sa=${_bst_del_ref[sa]}
+    local _bst_del_sa_ret=0
+    local _bst_del_tmp_id=
+
+    if ((_bst_del_current)) ; then
+        "$_bst_del_sa" "${_bst_del_ref[${_bst_del_current}.value]}" "$_bst_del_value"
+        _bst_del_sa_ret=$?
+    fi
+
+    while ((_bst_del_current)) && ((_bst_del_sa_ret!=2)) ; do
+        _bst_del_parent=$_bst_del_current
+        "$_bst_del_sa" "${_bst_del_ref[${_bst_del_current}.value]}" "$_bst_del_value"
+        case "$?" in
+        0)  _bst_del_current=${_bst_del_ref[${_bst_del_current}.left]}
+            _bst_del_is_l_child=1 ;;
+        1)  _bst_del_current=${_bst_del_ref[${_bst_del_current}.right]}
+            _bst_del_is_l_child=0 ;;
+        esac
+    done
+
+    ((_bst_del_current)) || {
+        _bst_del_out_id=$_bst_del_root_id
+        return ${BST_COMMON_ERR_DEFINE[del_not_found]}
+    }
+
+    # 叶子节点
+    if ! ((_bst_del_ref[${_bst_del_current}.left])) &&
+        ! ((_bst_del_ref[${_bst_del_current}.right])) ; then
+        # 如果删除的是根节点并且没有子节点,树为空
+        if ((_bst_del_current==_bst_del_root_id)) ; then
+            _bst_del_node "$1" "$_bst_del_root_id"
+            _bst_del_out_id=0
+            return ${BST_COMMON_ERR_DEFINE[del_root_node]}
+        fi
+    elif ! ((_bst_del_ref[${_bst_del_current}.left])) &&                                                             
+        # 只有一个右子节点                                               #   (parrent)30            (parrent)30      
+        _bst_del_tmp_id=${_bst_del_ref[${_bst_del_current}.right]}       #            |                      | 
+        if ((_bst_del_current==_bst_del_root_id)) ; then                 #    .-------'----.         .-------'----.  
+            # 释放根节点                                                 #    |       .    |         |       .    |  
+            _bst_del_out_id=${_bst_del_tmp_id}                           #    |       .              |       .    |  
+            _bst_del_node "$1" "$_bst_del_root_id"                       #cur 20(X)   .   50         20      .   50(X) current
+            return ${BST_COMMON_ERR_DEFINE[ok]}                          #    '---.   .          .---'---.   .    '-----.
+        fi                                                               #        |   .          |       |   .          |
+                                                                         #        25 <'         15       25  '.........>70
+        if ((_bst_del_is_l_child)) ; then
+            _bst_del_ref[${_bst_del_parent}.left]=${_bst_del_tmp_id}
+            _bst_del_ref[${_bst_del_tmp_id}.parent_side]='left'
+        else
+            _bst_del_ref[${_bst_del_parent}.right]=${_bst_del_tmp_id}
+            _bst_del_ref[${_bst_del_tmp_id}.parent_side]='right'
+        fi
+        _bst_del_ref[${_bst_del_tmp_id}.parent]=$_bst_del_parent
+
+        # 释放当前节点内存
+        _bst_del_node "$1" "$_bst_del_current"
+    elif ! ((_bst_del_ref[${_bst_del_current}.right])) &&                                                             
+        # 只有一个左子节点                                               #       (parrent)30            (parrent)30      
+        _bst_del_tmp_id=${_bst_del_ref[${_bst_del_current}.left]}        #                |                      | 
+        if ((_bst_del_current==_bst_del_root_id)) ; then                 #        .-------'----.         .-------'----------.
+            # 释放根节点                                                 #        |       .    |         |       .          |
+            _bst_del_out_id=${_bst_del_tmp_id}                           #        |       .              |       .          |
+            _bst_del_node "$1" "$_bst_del_root_id"                       #    cur 20(X)   .   50         20      .          50(X) current
+            return ${BST_COMMON_ERR_DEFINE[ok]}                          #    .---'       .          .---'---.   .          |
+        fi                                                               #    |           .          |       |   .       .--'    
+                                                                         #   15<..........'         15       25  .       |       
+        if ((_bst_del_is_l_child)) ; then                                #                                       '.....>40       
+            _bst_del_ref[${_bst_del_parent}.left]=${_bst_del_tmp_id}
+            _bst_del_ref[${_bst_del_tmp_id}.parent_side]='left'
+        else                                                             #
+            _bst_del_ref[${_bst_del_parent}.right]=${_bst_del_tmp_id}    #              30(->35)current    delete node 30
+            _bst_del_ref[${_bst_del_tmp_id}.parent_side]='right'         #              |
+        fi                                                               #       .------'------.
+        _bst_del_ref[${_bst_del_tmp_id}.parent]=$_bst_del_parent         #       |      ^      |
+                                                                         #      20      .      50
+        # 释放当前节点内存                                               #       |      .      |
+        _bst_del_node "$1" "$_bst_del_current"                           #       '---.  .  .---'---.
+    fi                                                                   #           |  .  |       |
+                                                                         #           25 '.>35(X)   60
+}                                                                        #           |    (->30)   |
+                                                                         #       .---'             '--.
+return 0                                                                 #       |                    |
+                                                                         #       24                   70
+                                                                         #
+
+
+
+
+
+
+
+
+
+
+
+
 
